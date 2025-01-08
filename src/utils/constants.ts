5371b8ec6e87b4d254e7a282dd58d431bc0e1dbf
Willy Ghozali <willyghozali@gmail.com>
2025-01-08 20:30:48 +0700

    add faq section

---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const whatsappBookingUrl =
+  "https://wa.me/6287771616166?text=Halo,%20saya%20ingin%20memesan%20sesi.";
+
+export const locationUrl =
+  "https://www.google.com/maps/place/Calia+Japanese+Head+Spa+Gading+Serpong/@-6.2651702,106.6066837,17z/data=!4m6!3m5!1s0x2e69fd052eb2e35b:0xcfc9cfb821d57d7b!8m2!3d-6.2651702!4d106.6066837!16s%2Fg%2F11wjjpcjn9?entry=ttu&g_ep=EgoyMDI1MDEwMi4wIKXMDSoASAFQAw%3D%3D";
